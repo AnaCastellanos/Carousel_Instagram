bb267c352456d405f2a2e95e228016a05255fda7
Ana Castellanos <ana.lec15@gmail.com>
2018-11-06 15:34:01 -0600

    First commit

--- src/app/carrete/carrete.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component ({
-  selector: 'app-carrete',
-  templateUrl: './carrete.component.html',
-  styleUrls: ['./carrete.component.css']
-})
-export class CarreteComponent {
-}
