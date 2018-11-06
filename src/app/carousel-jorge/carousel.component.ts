229c2bbc4289ea7db311cad6ef40c6dab9e4b929
Ana Castellanos <ana.lec15@gmail.com>
2018-11-06 16:30:39 -0600

    optimization code

--- src/app/carousel-jorge/carousel.component.ts
+++ src/app/carousel-jorge/carousel.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import {InstagramService} from '../instagram.service';
+import { Component } from '@angular/core';
+import { InstagramService } from '../instagram.service';
 
 @Component ({
   selector: 'app-carousel-jorge',
@@ -7,9 +7,7 @@ import {InstagramService} from '../instagram.service';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  images2: string [] = [];
-
+  images: any [] = [];
 
   constructor(public instaService: InstagramService) {
     this.getImages();
@@ -17,10 +15,9 @@ export class CarouselComponent {
 
   getImages() {
     this.instaService.getData().subscribe( data => {
-      for (let i = 0; i < 3; i++) {
-        this.images2[i] = data.data[i].images.standard_resolution.url;
-      }
-    });
+      for(let image of data.data)
+        this.images.push(image);
+    }, error => console.log(error));
   }
 
 }
