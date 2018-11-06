0ef1a5973b65e0e0dfbd4028b5f74cf182e4c88d
jorgepinazero <jorge.pina@polimentes.mx>
2018-11-06 11:13:04 -0600

    service now working-jorge

--- src/app/instagram.service.ts
+++ src/app/instagram.service.ts
@@ -1,8 +1,26 @@
-import {Observable} from 'rxjs';
-import {Input} from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import {Http, Response} from '@angular/http';
+import {map} from 'rxjs/operators';
+import {Injectable} from '@angular/core';
 
+@Injectable()
 export class InstagramService{
-@Input() url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=4536554789.0dc7b40.fb494b1d649f4b48ab78b0afcf5bdaa7';
+  private apiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=4536554789.0dc7b40.fb494b1d649f4b48ab78b0afcf5bdaa7';
+  data: any = {};
+
+  constructor(public http: Http) {
+    this.getData();
+    this.importData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+      .pipe(map ((res: Response) => res.json()));
+  }
+
+  importData() {
+    this.getData().subscribe(data => {
+      this.data = data;
+    });
+  }
 
 }
